80024db389e4163898c56fba6a054212415e71d3
Eduard Aymerich <eduardaymerich@gmail.com>
2021-11-20 02:27:10 +0100

    rollback to post request

--- components/Tournaments.vue
+++ components/Tournaments.vue
@@ -55,7 +55,7 @@ export default {
         this.loading = true
 
         await this.$axios
-            .get(this.$config.apiURL, this.getParams)
+            .post(this.$config.apiURL, this.getParams)
             .then((response) => {
                 this.tournaments = response.data.tournaments
                 this.loading = false
